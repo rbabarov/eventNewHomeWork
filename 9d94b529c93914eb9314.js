ca4d1b4ff05937ba59ab4cf968f8b7602e1885b0
AppVeyor <ci@appveyor.com>
2023-01-24 17:32:08 +0000

    1701143

---
+++ 9d94b529c93914eb9314.js
@@ -0,0 +1,1 @@
+import Game from"./Game";import IndexGoblin from"./IndexGoblin";const gamesBoard=new Game;gamesBoard.createBoard(16);const indGob=new IndexGoblin;indGob.renderApp(),indGob.gameClick();
